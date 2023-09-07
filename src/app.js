368e3fbbae52d028f3995b3e4137041624a14222
sm-nat <141369048+sm-nat@users.noreply.github.com>
2023-09-07 03:16:07 +0000

    last commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "Aliens"];
+  let action = ["ate", "peed", "crushed", "broke", "stole"];
+  let what = ["my homework", "the keys", "the car", "my computer"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+    "in a parallel universe"
+  ];
+
+  let whoindex = Math.floor(Math.random() * who.length);
+  let actionindex = Math.floor(Math.random() * action.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+  let whenindex = Math.floor(Math.random() * when.length);
+
+  return `${who[whoindex]} ${action[actionindex]} ${what[whatindex]} ${when[whenindex]}`;
 };
